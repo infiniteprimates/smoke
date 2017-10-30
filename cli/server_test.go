9db0feda55d9121abf3b57390ddd7243fb86850b
Kenneth Rawlings <rawlink@users.noreply.github.com>
2017-10-30 07:17:49 -0700

    Feature/ng and wercker (#1)
    * Add angular project. Update base .gitignore file
    
    * Add wercker and initial test functionality. Tests are *very* rudimentary.
    
    * Update dep info
    
    * Update readme with badges and quick description.

---
+++ cli/server_test.go
@@ -0,0 +1,13 @@
+package cli
+
+import (
+	"testing"
+	
+	"github.com/stretchr/testify/assert"
+)
+
+func TestServer_runServer_Success(t *testing.T) {
+	err := runServer(nil, nil);
+
+	assert.NoError(t, err, "An error occured running the server CLI.")
+}
